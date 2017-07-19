deca4579b18a6a688577c944f04ce0e34e0dd28d
weishao6hao <798364373@qq.com>
2017-07-19 10:54:17 +0800

    Delete some files

--- Sampling-Algorithm（C++）/ADN_Esclave2.h
+++
@@ -1,8 +0,0 @@
-#ifndef ADNESCLAVE2_H
-#define ADNESCLAVE2_H
-
-void InitSingleParamFCNNrule(ParamFCNNrule *P);
-void InitSingleNull(ParamFCNNrule *P);
-ParamFCNNrule *InitParamFCNNrule(int n);
-void LibereParamFCNNrule(struct ParamFCNNrule *P);
-#endif 
--- Sampling-Algorithm（C++）/DENDIS.h
+++
@@ -1,4 +0,0 @@
-#ifndef DENDIS_H
-#define DENDIS_H
-int SamplingApproachP_DENDIS(double **data,int n,int p,double Granularity,double **prototype,int &N_instance,int *weight, char *chemin,int *option);	
-#endif
